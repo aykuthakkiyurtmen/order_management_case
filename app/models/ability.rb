39dd2d74098daf6859c6751a05cf3234db863dee
aykutyurtmen <aykutyurt19907@gmail.com>
2022-07-24 15:44:28 +0300

    authorization is added

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.admin?
+      can :manage, :all
+    elsif user.regular?
+      can :read, Store
+      can :read, Product
+      can :manage, Favorite
+    end
+  end
+end
